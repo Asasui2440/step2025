4f131040a1a708bca60c373e389e3f31101b486c
Asasui2440 <nekocat3838@gmail.com>
2025-06-14 08:44:58 +0900

    連結リストで書いた

--- lec02_hash_table/hw4_cache.py
+++ lec02_hash_table/hw4_cache.py
@@ -1,16 +1,25 @@
 from __future__ import annotations
 from typing import Optional
-
+# from hw1_hash_table import put
 
 # Implement a data structure that stores the most recently accessed N pages.
 # See the below test cases to see how it should work.
 #
 # Note: Please do not use a library like collections.OrderedDict). The goal is
 #       to implement the data structure yourself!
+
+
 class Page:
-    def __init__(self: Page, url: str, contents: str, next: Optional[Page]):
+    def __init__(
+        self: Page,
+        url: str,
+        contents: str,
+        prev: Optional[Page] = None,
+        next: Optional[Page] = None,
+    ):
         self.url = url
         self.contents = contents
+        self.prev = prev
         self.next = next
 
     # これは確認用
@@ -26,9 +35,11 @@ class Cache:
     # Initialize the cache.
     # |n|: The size of the cache.、
     def __init__(self: Cache, n: int):
-        self.cache = None
+        self.head = None  # 先頭のポインタ
+        self.tail = None  # 末尾のポインタ
         self.cache_size = n
         self.size = 0
+        self.hash_table = {}
 
     # Access a page and update the cache so that it stores the most recently
     # accessed N pages. This needs to be done with mostly O(1).
@@ -36,30 +47,48 @@ class Cache:
     # |contents|: The contents of the URL
     def access_page(self: Cache, url: str, contents: str) -> None:
         # ------------------------#
-        prev_page = None
-        page = self.cache
-        while page:
-            if page.url == url:  # find page in cache
-                self.size -= 1
-                if prev_page:
-                    prev_page.next = page.next
-                else:
-                    self.cache = page.next
-                break
-            prev_page = page
-            page = page.next
+        if (url, contents) in self.hash_table:
+            page = self.hash_table[(url, contents)]
+            if page is self.head:  # 先頭のページだったらそのまま返す
+                return
 
-        new_page = Page(url, contents, self.cache)
-        self.cache = new_page
-        self.size += 1
+            # ページを一旦削除
+            if page.prev:
+                page.prev.next = page.next
+            if page.next:
+                page.next.prev = page.prev
+            if page is self.tail:
+                self.tail = page.prev
+
+            # ページを先頭に持ってくる
+            page.next = self.head
+            page.prev = None
+            if self.head:
+                self.head.prev = page
+            self.head = page
+
+        else:
+            # keyがハッシュテーブルになければ新たに作る
+            page = Page(url, contents, None, None)
+            self.hash_table[(url, contents)] = page
+            if self.head is None:
+                self.head = page
+            else:
+                page.next = self.head
+                page.prev = None
+                self.head.prev = page
+                self.head = page
+
+            if self.tail is None:
+                self.tail = page
+
+            self.size += 1
+
         if self.size > self.cache_size:
-            prev_page = None
-            page = self.cache
-            while page.next:  # try to find the last page
-                prev_page = page
-                page = page.next
-
-            prev_page.next = None
+            del self.hash_table[(self.tail.url, self.tail.contents)]
+            if self.tail.prev:
+                self.tail.prev.next = None
+            self.tail = self.tail.prev
             self.size -= 1
 
         # ------------------------#
@@ -69,11 +98,13 @@ class Cache:
     def get_pages(self: Cache) -> list:
         # ------------------------#
         pages = []
-        page = self.cache
-        while page:
-            pages.append(page.url)
-            page = page.next
+        pointer = self.head
+
+        while pointer:
+            pages.append(pointer.url)
+            pointer = pointer.next
         # ------------------------#
+        # print(pages)
         return pages
 
 
@@ -167,3 +198,8 @@ def cache_test():
 
 if __name__ == "__main__":
     cache_test()
+
+
+# urlによってページが違うものがあった場合は？
+# urlに対して、ポインタを持っておくor 双方向リストで考える。
+# url ポインタ prev_page
